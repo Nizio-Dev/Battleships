24f0ab065c01f0ad9981241b5fc0e0aa9addb845
Nizio-Dev <nizio.dev@gmail.com>
2022-02-25 23:04:54 +0100

    Smart-er algorithm works.

--- Battleships/Strategies/SmarterStrategy.cs
+++ Battleships/Strategies/SmarterStrategy.cs
@@ -1,6 +1,5 @@
 ﻿
 using Battleships.Interfaces;
-
 namespace Battleships.Strategies;
 
 enum Direction
@@ -18,8 +17,7 @@ public class SmarterStrategy : IStrategy
     private int _initialHitX = 0;
     private int _initialHitY = 0;
 
-    private int _targetHitX = 0;
-    private int _targetHitY = 0;
+    private bool _hasHit = false;
 
     private bool _knownShipLocation = false;
 
@@ -38,199 +36,148 @@ public class SmarterStrategy : IStrategy
         .Select(x => 
             Enumerable.Range(0, Player.Map.GameMap.Count).ToList())
         .ToList();
-        /*
-        int mapSize = Player.Map.GameMap.Count;
-
-        List<List<int>> hits = Player.Hits;
-
-         hits = Enumerable.Range(0, mapSize)
-        .Select(x => 
-            Enumerable.Range(0, mapSize).Where(i => i % 2 == 0).ToList())
-        .ToList();
-
-        for (int i = 0; i < hits.Count; i+=2)
-        {
-            hits[i] = hits[i].Select(c => c+1).ToList();
-        }
-
-        Player.Hits = hits;*/
     }
 
     public void Attack()
     {
-
-        CheckForShips();
-
-        if (!_knownShipLocation)
+        if (!_knownShipLocation || _hasSankShipLastTurn)
         {
-            HitNextUnknown();
-            _direction = Direction.Up;
-            return;
+            HitNextUnknown();  
+
         }
-
-
-
-        Move();
-
-    
-
-    }
-
-    private void CheckForShips()
-    {
-
+        else
+        {
+            Move();
+        }
     }
 
     private void Move()
     {
+        _hasHit = false;
 
-        DirectionBoundary();
-
-        switch (_direction)
+        while (!_hasHit)
         {
-            case Direction.Up:
-                Hit(0, -1);
-            break;
-            case Direction.Down:
-                Hit(0, 1);
-            break;
-            case Direction.Left:
-                Hit(-1, 0);
-            break;
-            case Direction.Right:
-                Hit(1, 0);
-            break;
+
+            switch (_direction)
+            {
+                case Direction.Up:
+                    TryHit(_initialHitX, _initialHitY-_consuctiveHits);
+                break;
+                case Direction.Down:
+                    TryHit(_initialHitX, _initialHitY+_consuctiveHits);
+                break;
+                case Direction.Left:
+                    TryHit(_initialHitX-_consuctiveHits, _initialHitY);
+                break;
+                case Direction.Right:
+                    TryHit(_initialHitX+_consuctiveHits, _initialHitY);
+                break;
+            }
         }
     }
 
 
-    private void Hit(int vectorX, int vectorY)
+    private void TryHit(int coordX, int coordY)
     {
-        int coordX = _targetHitX+vectorX;
-        int coordY = _targetHitY+vectorY;
 
-        Console.WriteLine($"{coordX} {coordY}");
+        if (!IsHitValid(coordX, coordY))
+        {
+            SetDirection();
+            _consuctiveHits=1;
+            return;
+        }
+        
+        Hit(coordX, coordY);
+        _hasHit = true;
+        return;
+
+    }
+
+    private void Hit(int coordX, int coordY)
+    {
 
         if(Player.Map.Hit(coordX, coordY))
         {
-            _consuctiveHits++;
             if(Player.Map.GameMap[coordX][coordY].Ship.Health <= 0)
             {
                 _knownShipLocation = false;
             }
+            Player.Hits[coordX].Remove(coordY);
+            _consuctiveHits++;
         }
         else
         {
-            _targetHitX = _initialHitX;
-            _targetHitY = _initialHitY;
-            SetDirection();
-            DirectionBoundary();
+            _consuctiveHits = 0;
         }
-        Player.Hits[coordX].Remove(coordY);
+    
     }
-
 
     private void SetDirection()
     {
         switch (_direction)
         {
             case Direction.Up:
-                _direction = Direction.Down;
+                _direction = _consuctiveHits > 1 ? Direction.Down : Direction.Right;
                 break;
 
             case Direction.Down:
-                _direction = Direction.Left;
+                _direction = _consuctiveHits > 1 ? Direction.Up : Direction.Left;
                 break;
 
             case Direction.Left:
-                _direction = Direction.Right;
+                _direction = _consuctiveHits > 1 ? Direction.Right : Direction.Up;
                 break;
 
             case Direction.Right:
-                _direction = Direction.Up;
+                _direction = _consuctiveHits > 1 ? Direction.Left : Direction.Down;
                 break;
         }
     }
 
 
-        private void DirectionBoundary()
+    private bool IsHitValid(int targettedX, int targettedY)
     {
 
-        var mapSize = Player.Map.GameMap.Count;
-        switch (_direction)
+        var map = Player.Map.GameMap;
+
+        if(targettedX >= map.Count || targettedX < 0 ||  targettedY >= map.Count || targettedY < 0)
         {
-            case Direction.Up:
-                if(_targetHitY+1 > mapSize) _direction = Direction.Down;
-            break;
-
-            case Direction.Down:
-                if(_targetHitY-1 <= 0) _direction = Direction.Left;
-            break;
-
-            case Direction.Left:
-                if(_targetHitX-1 <= 0) _direction = Direction.Right;
-            break;
-
-            case Direction.Right:
-                if(_targetHitX+1 > mapSize) _direction = Direction.Up;
-            break;    
+            return false;
         }
-    }
+        if(map[targettedX][targettedY].FieldSign != 'O')
+        {
+            return false;
+        }
+        return true;
 
+    }
 
     private void HitNextUnknown()
     {
-
         int x;
         do
         {
             x = _random.Next(0, Player.Hits.Count);
-        }while(Player.Hits[x].Count <= 0); // Z listy list dostępnych pól do zbicia losuje którąś listę.
-                                           // Odpowiada ona za pierwszą współrzędną.
+        }while(Player.Hits[x].Count <= 0);
 
-        var YValue = _random.Next(0, Player.Hits[x].Count); // Wybiera liczbę z listy, jest to druga współrzędna
+        var YValue = _random.Next(0, Player.Hits[x].Count);
 
         var y = Player.Hits[x][YValue];
 
-        if(Player.Map.Hit(x, y)) // Uderza w punkt na mapie
+        if(Player.Map.Hit(x, y))
         {
             _knownShipLocation = true;
             _initialHitX = x;
-            _initialHitY =  y;
-            _targetHitX = x;
-            _targetHitY = y;
+            _initialHitY = y;
             _consuctiveHits++;
             if(Player.Map.GameMap[x][y].Ship.Health <= 0)
             {
-                _knownShipLocation = false;
+                _hasSankShipLastTurn = true;
             }
         } 
         
-        Player.Hits[x].Remove(y); // Usuwa liczbę z listy
-
-        /*var currentCol = Player.Hits.First(i => i.Count > 0);
-
-        var currentColIdx =  Player.Hits.IndexOf(currentCol);
-
-        Console.WriteLine($"{currentColIdx} - {currentCol[0]}");
-
-        if(Player.Map.Hit(currentColIdx, currentCol[0]))
-        {
-            _knownShipLocation = true;
-            _initialHitX = currentColIdx;
-            _initialHitY =  currentCol[0];
-        }
-        else
-        {
-            _knownShipLocation = false;
-        }
-
-        currentCol.RemoveAt(0);*/
+        Player.Hits[x].Remove(y);
     }
 
-    private void FindHitField()
-    {
-        
-    }
 
 }
